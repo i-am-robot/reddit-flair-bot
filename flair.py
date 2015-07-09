445db02f52e84b318714301814493f5727e83c50
i-am-robot <alpag12@gmail.com>
2015-07-09 15:40:12 -0700

    :D

--- flair.py
+++ flair.py
@@ -6,15 +6,15 @@ import sqlite3
 
 '''USER CONFIGURATION'''
 
-TRIGGER_PHRASES = ['Legit!', 'OP is legit'] # Not case sensitive
-REPLY_MESSAGE = 'Changed flair!'
+TRIGGER_PHRASES = ['Legit', 'Legit!', 'OP is legit'] # Not case sensitive
+REPLY_MESSAGE = 'Marked /u/__OP__ as legit, thank you!'
 
 USERNAME  = "Beep-Boop-I-Am-Robot"
-PASSWORD  = "redacted"
+PASSWORD  = ""
 
 USERAGENT = "Python Flair Bot by /u/wildmonkeys"
 
-SUBREDDIT = "monkeybottesting"
+SUBREDDIT = ""
 
 MAXPOSTS = 100
 
@@ -41,45 +41,43 @@ r = praw.Reddit(USERAGENT)
 r.login(USERNAME, PASSWORD) 
 
 def flairbot():
-	print('Cycle #%i' % numCycles)
+    print('Cycle #%i' % numCycles)
     print('Searching %s.' % SUBREDDIT)
     subreddit = r.get_subreddit(SUBREDDIT)
     posts = list(subreddit.get_comments(limit=MAXPOSTS))
     posts.reverse()
     for post in posts:
-        # Anything that needs to happen every loop goes here.
         pid = post.id
 		
         try:
             pauthor = post.author.name
         except AttributeError:
-            # Author is deleted. We don't care about this post.
+            # Author is deleted
             continue
-
-        #if pauthor.lower() in [USERNAME.lower()]:
-            # Don't reply to yourself, robot!
-        #    continue
 
         cur.execute('SELECT * FROM oldposts WHERE ID=?', [pid])
         if cur.fetchone():
-            #print('already in database')
+            # Already in database
             continue
 
         cur.execute('INSERT INTO oldposts VALUES(?)', [pid])
         sql.commit()
         pbody = post.body.lower()
         if any(phrase.lower() == pbody for phrase in TRIGGER_PHRASES):
-            print('Triggered')
-            print(post.submission.author.name)
-            r.set_flair(SUBREDDIT, post.submission.author.name, 'LEGIT')
-            post.reply(REPLY_MESSAGE)
+            op = post.submission.author.name
+            print('Triggered for /u/{}'.format(op))
+            r.set_flair(SUBREDDIT, op, 'LEGIT')
+			
+            replymsg = REPLY_MESSAGE
+            replymsg = replymsg.replace('__OP__', op)
+            post.reply(replymsg)
 
 cycles = 0
 while True:
     try:
         flairbot()
         cycles += 1
-		numCycles +=1
+        numCycles +=1
     except Exception as e:
         traceback.print_exc()
     if cycles >= CLEANCYCLES:
@@ -89,5 +87,3 @@ while True:
         cycles = 0
     print('Running again in %d seconds \n' % WAIT)
     time.sleep(WAIT)
-
-    
